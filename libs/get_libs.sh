c27bfcdbef13cfc28f6c0bb8bc54665c5a2172cb
ritsch <ritsch@algo.mur.at>
2017-12-19 15:10:33 +0100

    cleaned structure

---
+++ libs/get_libs.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+cd $(dirname $0)
+
+needed_libs="escher escherFET16 escherOS pde pdpp"
+
+base_path=algorythmics/
+
+
+# use github synthax
+base_url=https://github.com/
+base_git=git@github.com:
+
+for lib in $needed_libs
+do
+    if  [ ! -d $lib  ]; then
+        echo git checkout $lib
+        git clone $base_git$base_path$lib.git
+        if [ $? -ne 0 ]; then
+            git clone $base_url$base_path$lib.git
+        fi
+    else
+        git -C $lib pull
+    fi
+done
